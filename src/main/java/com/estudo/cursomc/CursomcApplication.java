2d2f66db03424df5e0a3294049fcc9e0360888ab
Miranda <matheus.300miranda@live.com>
2020-01-18 23:15:42 -0300

    Rest testado

--- src/main/java/com/estudo/cursomc/CursomcApplication.java
+++ src/main/java/com/estudo/cursomc/CursomcApplication.java
@@ -2,8 +2,12 @@ package com.estudo.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.estudo.cursomc.resources.CategoriaResource;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = {CategoriaResource.class})
 public class CursomcApplication {
 
 	public static void main(String[] args) {
