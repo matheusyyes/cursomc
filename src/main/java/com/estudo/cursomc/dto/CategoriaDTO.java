e5757cac4431c3abb8c40a29562ea640a434d87d
Miranda <matheus.300miranda@live.com>
2020-03-21 20:06:17 -0300

    Validação sintatica com Bean validation

--- src/main/java/com/estudo/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/estudo/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,19 @@ package com.estudo.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.estudo.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento Obrigátrio")
+	@Length(min = 5, max = 80, message = "O tamanho dever ser entre 5 a 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
--- src/main/java/com/estudo/cursomc/services/CategoriaService.java
+++ src/main/java/com/estudo/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.estudo.cursomc.domain.Categoria;
+import com.estudo.cursomc.dto.CategoriaDTO;
 import com.estudo.cursomc.repositories.CategoriaRepository;
 import com.estudo.cursomc.services.exception.DataIntegrityException;
 import com.estudo.cursomc.services.exception.ObjectNotFoundException;
@@ -63,4 +64,9 @@ public class CategoriaService {
 		return repo.findAll(pageRequest);
 	}
 	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		
+		return new Categoria(objDTO.getId(), objDTO.getNome());
+		
+	}
 }
