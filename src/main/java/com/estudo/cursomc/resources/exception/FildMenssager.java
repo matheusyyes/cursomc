18efb54f59fa64e73f088a00b58e1f00c1cd2447
Miranda <matheus.300miranda@live.com>
2020-03-27 17:25:16 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/estudo/cursomc/resources/exception/FildMenssager.java
@@ -0,0 +1,39 @@
+package com.estudo.cursomc.resources.exception;
+
+import java.io.Serializable;
+
+public class FildMenssager implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String fieldName;
+	private String message;
+	
+	public FildMenssager() {
+		
+	}
+
+	public FildMenssager(String fieldName, String message) {
+		super();
+		this.fieldName = fieldName;
+		this.message = message;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
---
+++ src/main/java/com/estudo/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,26 @@
+package com.estudo.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+
+	private List<FildMenssager> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<FildMenssager> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FildMenssager(fieldName, message));
+	}
+
+	
+
+}
