bc382ecc2c17b5fb54c217712d46d0247eefd0eb
Miranda <matheus.300miranda@live.com>
2020-04-21 19:36:02 -0300

    Validação customizada de Email, corrigido erro de validação de cpfOuCnpj

--- src/main/java/com/estudo/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/estudo/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.estudo.cursomc.domain.Cliente;
 import com.estudo.cursomc.domain.enums.TipoCliente;
 import com.estudo.cursomc.dto.ClienteNewDTO;
+import com.estudo.cursomc.repositories.ClienteRepository;
 import com.estudo.cursomc.resources.exception.FildMenssager;
 import com.estudo.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -25,14 +32,20 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipoCliente().equals(TipoCliente.PESSOAFISICA.getCod())
 				&& !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 
-			list.add(new FildMenssager("cpfOuCNPJ", "Cpf inválido"));
+			list.add(new FildMenssager("cpfOuCnpj", "Cpf inválido"));
 
 		}
 		if (objDto.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCod())
 				&& !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 
-			list.add(new FildMenssager("cpfOuCNPJ", "CNPJ inválido"));
+			list.add(new FildMenssager("cpfOuCnpj", "CNPJ inválido"));
 
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if(aux != null) {
+			list.add(new FildMenssager("email","Email ja existente"));
 		}
 
 		for (FildMenssager e : list) {
